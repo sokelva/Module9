e6fd99e2d6aead7b3e4ddc5c9b18153e0af7058a
sokelva <sokelva@mail.ru>
2023-01-09 11:37:42 +0300

    Делегаты - скринкаст.

--- Exeptions/Program.cs
+++ Exeptions/Program.cs
@@ -4,43 +4,81 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Exeptions
+namespace _9._3
 {
-    class Program
+    /// <summary>
+    /// Делегаты и их прктическое применение
+    /// </summary>
+    public class Program
     {
-        public delegate void ShowDelegate();
+        public delegate bool EligibleToPromotion(Employee EmployeeToPromotion);
 
         static void Main(string[] args)
         {
+            Employee empl1 = new Employee()
+            {
+                ID = 1,
+                Name = "Алексей",
+                Experience = 5,
+                Salary = 20000
+            };
+            Employee empl2 = new Employee()
+            {
+                ID = 2,
+                Name = "Михаил",
+                Experience = 2,
+                Salary = 10000
+            };
+            Employee empl3 = new Employee()
+            {
+                ID = 3,
+                Name = "Сергей",
+                Experience = 3,
+                Salary = 15000
+            };
 
-            ShowDelegate showDelegate = ShowMessage1;
-            showDelegate += ShowMessage2;
-            showDelegate += ShowMessage3;
-            showDelegate += ShowMessage4;
+            List<Employee> ListEmployee = new List<Employee>();
+            ListEmployee.Add(empl1);
+            ListEmployee.Add(empl2);
+            ListEmployee.Add(empl3);
 
-            showDelegate.Invoke();
-
+            EligibleToPromotion eligibleToPromotion = Promote;
+            Employee.PromoteEmployee(ListEmployee, eligibleToPromotion);
             Console.ReadKey();
+
         }
 
-        static void ShowMessage1()
+        public static bool Promote(Employee employee)
         {
-            Console.WriteLine("Метод 1");
-        }
-
-        static void ShowMessage2()
-        {
-            Console.WriteLine("Метод 2");
-        }
-
-        static void ShowMessage3()
-        {
-            Console.WriteLine("Метод 3");
-        }
-
-        static void ShowMessage4()
-        {
-            Console.WriteLine("Метод 4");
+            if (employee.Salary > 10000)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            } 
         }
     }
+
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Experience { get; set; }
+        public int Salary { get; set; }
+
+        public static void PromoteEmployee(List <Employee> listEmployee, Program.EligibleToPromotion IsEmployeeEligible)
+        {
+            foreach (Employee employee in listEmployee)
+            {
+                if (IsEmployeeEligible(employee))
+                {
+                    Console.WriteLine("Сотрудник {0} повышен.", employee.Name);
+                }
+            }
+        }
+    }
+
+
 }
