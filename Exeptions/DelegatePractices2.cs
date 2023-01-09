9af7b120ef9342c85714c15b525b861c6674fcc1
sokelva <sokelva@mail.ru>
2023-01-09 11:51:42 +0300

    Задание 9.3.15

---
+++ Exeptions/DelegatePractices2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePractices3
+{
+    class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = RandomNumber;
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+
+        static int RandomNumber()
+        {
+            return new Random().Next(0, 100);
+        }
+    }
+}
+
+namespace DelegatePractices4
+{
+    class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = delegate
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
+
+
+namespace DelegatePractices5
+{
+    class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = () => //лямбда - оператор
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
