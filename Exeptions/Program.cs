a5412b5789af4c31b2d11fc8feecf40bdce17470
sokelva <sokelva@mail.ru>
2023-01-15 18:30:13 +0300

    9.6. Практикум - Задание 1 (Exeptions)

--- Exeptions/Program.cs
+++ Exeptions/Program.cs
@@ -4,81 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _9._3
+namespace OwnException
 {
-    /// <summary>
-    /// Делегаты и их практическое применение
-    /// </summary>
     public class Program
     {
-        public delegate bool EligibleToPromotion(Employee EmployeeToPromotion);
+        public static string[] ExeptionArray = new string[5] { "OverflowException", "PathTooLongException", "FormatException", "TimeoutException", "MyOwnExeption" };
 
         static void Main(string[] args)
         {
-            Employee empl1 = new Employee()
-            {
-                ID = 1,
-                Name = "Алексей",
-                Experience = 5,
-                Salary = 20000
-            };
-            Employee empl2 = new Employee()
-            {
-                ID = 2,
-                Name = "Михаил",
-                Experience = 2,
-                Salary = 10000
-            };
-            Employee empl3 = new Employee()
-            {
-                ID = 3,
-                Name = "Сергей",
-                Experience = 3,
-                Salary = 15000
-            };
 
-            List<Employee> ListEmployee = new List<Employee>();
-            ListEmployee.Add(empl1);
-            ListEmployee.Add(empl2);
-            ListEmployee.Add(empl3);
-
-            EligibleToPromotion eligibleToPromotion = Promote;
-            Employee.PromoteEmployee(ListEmployee, eligibleToPromotion);
-            Console.ReadKey();
-
-        }
-
-        public static bool Promote(Employee employee)
-        {
-            if (employee.Salary > 10000)
+            foreach (var item in ExeptionArray)
             {
-                return true;
-            }
-            else
-            {
-                return false;
-            } 
-        }
-    }
-
-    public class Employee
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Experience { get; set; }
-        public int Salary { get; set; }
-
-        public static void PromoteEmployee(List <Employee> listEmployee, Program.EligibleToPromotion IsEmployeeEligible)
-        {
-            foreach (Employee employee in listEmployee)
-            {
-                if (IsEmployeeEligible(employee))
+                try
                 {
-                    Console.WriteLine("Сотрудник {0} повышен.", employee.Name);
+                    throw new Exception(item.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Тип эксепшена: {0}", ex.Message);
+                }
+                finally
+                {
+                    CustomException ce = new CustomException("Сообщение базового класса!");
                 }
             }
         }
     }
 
-
+    class CustomException : Exception
+    {
+        public CustomException(string message) : base(message)
+        {
+            Exception ownException = new Exception("Собственное исключение!");
+            Console.WriteLine($"Собственное сообщение: {ownException.Message}");
+        }
+    }
 }
